ceb1a95aafb3b0735120682fdbd1d65978eff87f
Jun Hao, Garren <teejunhao@gmail.com>
2022-05-05 00:24:25 +0800

    New updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,12 +79,33 @@ let modal = function(modalClick) {
 }
 
 modalBtns.forEach((modalBtn, i) => {
-        modalBtn.addEventListener('click', () => {
-            modal(i)
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
         })
     })
-    /*==================== PORTFOLIO SWIPER  ====================*/
+})
 
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
